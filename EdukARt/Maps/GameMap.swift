d16572408aaf804f47e14c98d76654b3925b639b
Sina Steinmüller <sina_steinmller@gmx.de>
2026-08-13 22:00:24 +0200

    Map April tag in GameMap

--- EdukARt/Maps/GameMap.swift
+++ EdukARt/Maps/GameMap.swift
@@ -11,12 +11,33 @@ struct GameMap: Identifiable, Codable {
     var name: String
     let createdAt: Date
     
-    init(name: String) {
+    let referenceTagID: Int
+    var aprilTags: [MapAprilTag]
+    
+    
+    init(
+        name: String,
+        referenceTagID: Int,
+        aprilTags: [MapAprilTag]
+    ) {
         id = UUID()
         self.name = name
         createdAt = Date()
+        self.referenceTagID = referenceTagID
+        self.aprilTags = aprilTags
     }
 }
+
+
+struct MapAprilTag: Identifiable, Codable {
+    
+    let id: Int
+    
+    let x: Float
+    let y: Float
+    let z: Float
+}
+
 // ToDo April Tags
 // RoomOutline
 // Obstackles, Track, TrackWidth, PlacedItems
